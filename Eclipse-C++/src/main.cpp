5857a8ca1388800704cd2a727d18643f129d3c9c
Bas van Schaik <bas@traiectum.net>
2015-03-06 12:13:50 +0000

    add reference to LGPL 3 to main files
    (cherry picked from commit 3bf96d199618dffb5eabf634e77f87b100a91293)

--- Eclipse-C++/src/main.cpp
+++ Eclipse-C++/src/main.cpp
@@ -1,10 +1,20 @@
-//============================================================================
-// Name        : main.cpp (part of PaleoLatitude)
-// Author      : Sebastiaan J. van Schaik
-// Copyright   : Sebastiaan J. van Schaik
-// Licence     : GNU Lesser GPL 3.0
-// Description :
-//============================================================================
+/**
+Paleolatitude model Copyright (c) Sebastiaan J. van Schaik, All rights
+reserved.
+
+This library is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License (version 3.0) as published
+by the Free Software Foundation.
+
+This library is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+details.
+
+You should have received a copy of the GNU Lesser General Public License along
+with this library; if not, write to the Free Software Foundation, Inc., 51
+Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 #include "paleo_latitude/PaleoLatitude.h"
 #include "paleo_latitude/PLParameters.h"
 
