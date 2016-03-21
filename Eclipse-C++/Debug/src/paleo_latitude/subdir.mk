4e4752fe5c9718a87e16a8b6d178f3e5cec0976a
Bas van Schaik <bas@traiectum.net>
2016-03-21 02:52:46 +0000

    regenerated build scripts
    (cherry picked from commit 3d4286ae29a28a4707dc888398121f2c75f79e77)

--- Eclipse-C++/Debug/src/paleo_latitude/subdir.mk
+++ Eclipse-C++/Debug/src/paleo_latitude/subdir.mk
@@ -32,7 +32,7 @@ CPP_DEPS += \
 src/paleo_latitude/%.o: ../src/paleo_latitude/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -O0 -g3 -Wall -c -fmessage-length=0 -std=gnu++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -std=gnu++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- Eclipse-C++/Debug/tests/subdir.mk
+++ Eclipse-C++/Debug/tests/subdir.mk
@@ -26,7 +26,7 @@ CPP_DEPS += \
 tests/%.o: ../tests/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -O0 -g3 -Wall -c -fmessage-length=0 -std=gnu++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -std=gnu++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
